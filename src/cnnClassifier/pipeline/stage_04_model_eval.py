8aac5d0ea7b2512187de3205fd691abd99b7e672
Suraj Kale <surajkale99@gmail.com>
2025-04-15 15:36:16 +0530

    model folder created

--- src/cnnClassifier/pipeline/stage_04_model_eval.py
+++ src/cnnClassifier/pipeline/stage_04_model_eval.py
@@ -22,13 +22,13 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/surajkale99/DL_END_END_project.mlflow"
-        os.environ["MLFLOW_TRACKING_USERNAME"]="surajkale99"
-        os.environ["MLFLOW_TRACKING_PASSWORD"]="e4f2406488322cebc687e5c389841eb67f8beeec"
+        # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/surajkale99/DL_END_END_project.mlflow"
+        # os.environ["MLFLOW_TRACKING_USERNAME"]="surajkale99"
+        # os.environ["MLFLOW_TRACKING_PASSWORD"]="e4f2406488322cebc687e5c389841eb67f8beeec"
 
-        # Set MLflow tracking URI
-        mlflow.set_tracking_uri("https://dagshub.com/surajkale99/DL_END_END_project.mlflow")
-        evaluation.log_into_mlflow()
+        # # Set MLflow tracking URI
+        # mlflow.set_tracking_uri("https://dagshub.com/surajkale99/DL_END_END_project.mlflow")
+        # evaluation.log_into_mlflow()
 
 
 
